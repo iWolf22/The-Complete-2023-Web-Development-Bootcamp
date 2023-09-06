5d57913b20340a0f4699a92f2c8fe6f95bcc256e
Joshua Dierickse <joshua.dierickse@gmail.com>
2023-09-05 23:11:33 -0400

    Section 25

--- Section 25 - EJS/4.2 Passing Data/index.js
+++ Section 25 - EJS/4.2 Passing Data/index.js
@@ -6,9 +6,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  let data = {
+    header: "Enter your name below 👇" 
+  }
+  res.render("index.ejs", data);
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let data = {
+    header: "There are " + (req.body["fName"].length + req.body["lName"].length) + " letters in the name " + req.body["fName"] + " " + req.body["lName"]
+  }
+  res.render("index.ejs", data)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
