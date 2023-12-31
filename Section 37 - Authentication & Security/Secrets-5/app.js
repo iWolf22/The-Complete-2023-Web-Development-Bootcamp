eb1ba1b6f97a757791578c81b611e663ad1a2ee8
Joshua Dierickse <joshua.dierickse@gmail.com>
2023-12-30 21:30:55 -0500

    Auth

--- Section 37 - Authentication & Security/Secrets-5/app.js
+++ Section 37 - Authentication & Security/Secrets-5/app.js
@@ -9,7 +9,9 @@ import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
 import findOrCreate from "mongoose-findorcreate";
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy } from "passport-facebook";
+import { Strategy as GitHubStrategy } from "passport-github";
 
 const app = express();
 
@@ -30,7 +32,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String,
+    githubId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -40,19 +46,48 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id, email: profile.displayName, password:"Google OAuth" }, function (err, user) {
+            return cb(err, user);
+        });
+    } 
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    enableProof: true
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id, email: profile.displayName, password:"Facebook OAuth"  }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/secrets"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ githubId: profile.id, email: profile.displayName, password:"GitHub OAuth" }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
 app.get("/", async function(req, res) {
@@ -62,6 +97,36 @@ app.get("/", async function(req, res) {
 app.get("/auth/google",
     passport.authenticate("google", { scope: ["profile"]})
 );
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+        res.redirect("/secrets");
+    }
+);
+
+app.get("/auth/facebook",
+    passport.authenticate("facebook")
+);
+
+app.get("/auth/facebook/secrets",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    function(req, res) {
+        res.redirect("/secrets");
+    }
+);
+
+app.get("/auth/github",
+    passport.authenticate("github")
+);
+
+app.get("/auth/github/secrets", 
+    passport.authenticate("github", { failureRedirect: "/login" }),
+    function(req, res) {
+        res.redirect("/secrets");
+    }
+);
+
 
 app.get("/login", function(req, res) {
     res.render("login");
@@ -75,9 +140,14 @@ app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
 
-app.get("/secrets", function(req, res) {
+app.get("/secrets", async function(req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+
+        var findResult = await User.find({"secret": {$ne: null}});
+
+        console.log(findResult);
+
+        res.render("secrets", {userWithSecrets: findResult});
     } else {
         res.redirect("/login");
     }
@@ -117,4 +187,25 @@ app.post("/login", async function(req, res) {
 app.get("/logout", function(req, res) {
     req.logout(function() {});
     res.redirect("/");
+});
+
+app.get("/submit", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", async function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user._id);
+
+    var foundUser = await User.findById(req.user._id.toString());
+
+    foundUser.secret = submittedSecret;
+    foundUser.save();
+    res.redirect("/secrets");
+
 });
