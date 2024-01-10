5c736239d4d9539dceaf8e6c5a6d02a0de4eb5c0
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-01-09 20:14:29 -0500

    Section 39

--- Section 39 - MERN Stack/frontend/src/pages/Home.js
+++ Section 39 - MERN Stack/frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("/api/workouts");
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        }
+
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutForm />
         </div>
     );
 }
