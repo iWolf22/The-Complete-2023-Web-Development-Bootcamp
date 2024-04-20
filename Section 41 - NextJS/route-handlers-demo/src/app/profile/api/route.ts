9a437cd2679892a48c8a257652a1d3df9b0370bc
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-04-20 12:29:03 -0400

    NextJS Header Data

--- Section 41 - NextJS/route-handlers-demo/src/app/profile/api/route.ts
+++ Section 41 - NextJS/route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-    return new Response("API data!");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+
+    return new Response(
+        "<div><h1>API data!</h1><p>HTML formatted too!</p></div>",
+        {
+            headers: {
+                "Content-Type": "text/html",
+            },
+        }
+    );
 }
