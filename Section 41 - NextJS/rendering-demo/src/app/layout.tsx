000b73bdce3c233831940d273ef087f07b1e112c
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-04-21 15:27:45 -0400

    NestJS

--- Section 41 - NextJS/rendering-demo/src/app/layout.tsx
+++ Section 41 - NextJS/rendering-demo/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { createContext } from "react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
