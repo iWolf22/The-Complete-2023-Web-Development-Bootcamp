901eba410d0b767f93737675d2fedfd31677cc90
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-04-30 23:49:58 -0400

    authjs

---
+++ Section 41 - NextJS/06 - auth-tutorial/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <div>
+      <Button size="lg" variant="link">Click Me</Button>
+    </div>
+  );
+}
